ef54d615125a90a366f285929cb7351ce52f630f
es39 <esj10025@gmail.com>
2023-01-19 20:14:55 +0900

    Mydata add

---
+++ src/Data/MyData.js
@@ -0,0 +1,12 @@
+const es_data = [
+    {
+        name: 'EunSeon',
+        school: '상명대학교 컴퓨터과학전공',
+        mbti: 'INFJ',
+        want: '대체할 수 없는 개발자가 목표입니다',
+        github: 'https://github.com/es39',
+        insta: 'https://www.instagram.com/es.__.39/',
+    }
+]
+
+export { es_data }
